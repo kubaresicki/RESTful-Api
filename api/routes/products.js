3c182fcbbcbd4ac8e1d50681c204ea44ac9f8523
kubaresicki <you@example.com>
2020-01-22 18:45:27 +0100

    commit on master mongo

---
+++ api/routes/products.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose')
+
+const Product = require('../models/product');
+
+router.get('/',(req, res, next)=>{
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    })
+})
+
+router.get('/:productId',(req, res, next)=>{
+    const id = req.params.productId;
+    Product.findById(id)
+        .exec()
+        .then(doc =>{
+            console.log(doc);
+            res.status(200).json(doc)
+        })
+        .catch(err=> { 
+            console.log(err);
+            res.status(500).json({error: err});
+        });
+})
+
+
+router.post('/',(req, res, next)=>{
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+    product.save().then(result =>{
+        console.log(result);
+        })
+        .catch(err => console.log(err))
+        
+    res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: product
+    })
+})
+
+router.patch('/:productId', (req, res, next)=>{{
+        res.status(200).json({
+            message: 'updated product',
+            
+        })
+    }
+})
+
+
+router.delete('/:productId', (req, res, next)=>{{
+    res.status(200).json({
+        message: 'deleted product',
+        
+    })
+}
+})
+
+
+module.exports = router;
